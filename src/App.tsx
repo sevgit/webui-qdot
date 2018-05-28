e1519ced374f1ebd7cce8d7d374d34d360b06819
sevgit <esportsmarketer@gmail.com>
2018-05-28 13:26:13 -0300

    made better routing choices and added some safety nets to avoid rendering errors (i.e if someone manually accesses an unexistent comment /comment/12312312

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 
 import * as React from 'react';
-import { observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import CommentList from './containers/CommentList';
 import SingleComment from './components/SingleComment';
-
-
-@observer(['comments'])
+import ErrorFetching from './components/ErrorFetching';
+import Spinner from './components/Spinner';
+@inject('comments') @observer
 class App extends React.Component<any, any> {
+
 
   public componentDidMount() {
 
-    this.props.comments.fetchComments()
+     this.props.comments.fetchComments()
 
-
+    
   }
+
+ 
 
 
   public render() {
+    const listState = this.props.comments.state;
     return (
       <React.Fragment>
         <header>
@@ -27,15 +31,27 @@ class App extends React.Component<any, any> {
         <div className='App'>
         <BrowserRouter>
 
-          <Switch>
+          <Switch> 
             <Route exact={true} path="/comment/:id" render={(props) =>
-               this.props.comments.list.length > 0 && <SingleComment
+              {switch(this.props.comments.state) {
+                case 'initial': case 'loading':
+                return <Spinner loading={listState} />;
+                case 'done':
+                if (this.props.comments.list.length < props.match.params.id -1) {
+                  return <ErrorFetching buttonText={'Go Back'} buttonFunction={() => props.history.push('/')} />
+                }
+                return <SingleComment
                 postId={1}
                 id={this.props.comments.list[props.match.params.id - 1].id}
                 name={this.props.comments.list[props.match.params.id - 1].name}
                 email={this.props.comments.list[props.match.params.id - 1].email}
                 body={this.props.comments.list[props.match.params.id - 1].body}
-                expanded={true} /> } />
+                expanded={true} />;
+                default: 
+                   return null
+              }}
+            }
+                /> 
             <Route path="/" component={CommentList} /> 
 
           </Switch>
